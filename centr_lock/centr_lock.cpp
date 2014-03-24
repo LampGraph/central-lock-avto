f8bd589d8c2d8a76a94c00f34d64feae164db756
LampGraph <LampGraph@gmail.com>
2014-03-25 01:17:44 +0800

    new
    Signed-off-by: LampGraph <LampGraph@gmail.com>

---
+++ centr_lock/centr_lock.cpp
@@ -0,0 +1,17 @@
+/*
+ * centr_lock.cpp
+ *
+ * Created: 25.03.2014 1:15:27
+ *  Author: Vyacheslav
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
