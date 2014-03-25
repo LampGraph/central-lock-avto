cc4c1cf716c3f70f209e18127126f7d277f3011a
LampGraph <LampGraph@gmail.com>
2014-03-25 23:26:11 +0800

    interrupts added
    Signed-off-by: LampGraph <LampGraph@gmail.com>

--- centr_lock/centr_lock.cpp
+++ centr_lock/centr_lock.cpp
@@ -4,14 +4,32 @@
  * Created: 25.03.2014 1:15:27
  *  Author: Vyacheslav
  */ 
-
+#define F_CPU 1600000
+#define LED_PIN PB4
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+
+ISR(PCINT0_vect)
+{
+	PORTB |= (1<<4);
+}
 
 int main(void)
 {
+	DDRB = 0x00;
+	DDRB |= (1<<LED_PIN);	
+	SREG |= (1<<7); // √лобальное разрешение прерываний.
+	GIMSK |= (1<<PCIE); //–азрешаем прерывани€ по изменению пинов
+	PCMSK |= (1<<PCINT0)|(1<<PCINT1)|(1<<PCINT2);
     while(1)
     {
         //TODO:: Please write your application code 
+		_delay_ms(300);
+		PORTB &= ~(1<<4);
+//		_delay_ms(300);
+//		PORTB |= (1<<4);
     }
 }
